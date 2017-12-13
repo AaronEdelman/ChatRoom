a94b02ead4e07277cd409ebc809f023589e397b1
joe raleigh <joseph.m.raleigh@gmail.com>
2017-12-13 15:13:07 -0600

    dictionary class

---
+++ ChatroomStarter/Server/dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Dictionary
+    {
+        Dictionary<string, UserName> list = new Dictionary<string, UserName>();
+        UserName.Add("", key);
+        UserName[""]=Key;
+    }
+}
